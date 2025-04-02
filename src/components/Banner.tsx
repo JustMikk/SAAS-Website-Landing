bcd42fd5ea8e61a77cc5ac27f398367228bce724
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-04-02 21:54:52 +0300

    Add banner component with redesigned interface

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export const Banner = () => {
-  return null;
+  return (
+    <div className="text-center py-3 bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -
+          </span>{" "}
+          <Link href="#" className="underline underline-offset-4 ">
+            Explore the demo
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
