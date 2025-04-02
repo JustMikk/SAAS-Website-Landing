5efdf5cb0f4aa315ccb39ecc594b0b3a50d16700
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-04-02 23:30:23 +0300

    Add Hero component with updated content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,43 @@
+import Link from "next/link";
+import ArrowIcon from "../assets/icons/arrow-w.svg";
+
 export const Hero = () => {
-  return null;
+  return (
+    <div className="relative overflow-clip bg-gradient-to-b from-black via-indigo-900 to-purple-500 text-white py-[72px] sm:py-24">
+      <div className="absolute h-[375px] w-[750px] sm:h-[768px] sm:w-[1536px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB_)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+      <div className="container relative">
+        <div className="flex items-center justify-center">
+          <Link
+            href="#"
+            className="inline-flex gap-3 py-1 px-2 border border-white/30 border-dotted rounded-lg"
+          >
+            <span className="bg-gradient-to-r from-red-500 via-yellow-300 to-blue-500 text-transparent bg-clip-text [-webkit-background-clip:text]">
+              Version 2.0 is here
+            </span>
+            <span className="inline-flex gap-1 items-center">
+              <span>Read more</span>
+              <ArrowIcon />
+            </span>
+          </Link>
+        </div>
+        <div className="flex justify-center">
+          <h1 className="text-[min(10vw,128px)] mt-8 font-bold tracking-tighter leading-tight inline-flex text-center">
+            One Task <br />
+            at a time
+          </h1>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-xl text-center mt-8 max-w-md">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">
+            Get for free
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
