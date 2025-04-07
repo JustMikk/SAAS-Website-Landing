2f81958a87fdafe54ccc3a21b3882a2b22292761
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-04-07 17:08:28 +0300

    Add logo ticker component to display trusted logos

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,9 +1,11 @@
+"use client";
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +17,23 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world's most innovative teams.
+        </h2>
+        <div className="flex overflow-hidden gap-16 mt-9 before:content-[''] after:content-[''] before:absolute after:absolute relative before:h-full after:h-full before:w-5 after:w-5 before:top-0 after:top-0 before:left-0 after:right-0 before:bg-gradient-to-r from-black to-transparent after:bg-gradient-to-l">
+          {images.map(({ src, alt }) => (
+            <Image
+              key={alt}
+              src={src}
+              alt={alt}
+              className="flex-none h-8 w-auto"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
