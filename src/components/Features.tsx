f8cf9297778cad56feead002a4fc4eab8848ef0b
Mikiyas Hailegebriel <mikiyasbutu@gmail.com>
2025-04-07 18:04:54 +0300

    Add feature cards to Features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,3 +1,4 @@
+import EcosystemIcon from "../assets/icons/ecosystem.svg";
 const features = [
   {
     title: "Integration Ecosystem",
@@ -17,5 +18,33 @@ const features = [
 ];
 
 export const Features = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-center font-bold text-5xl sm:text-6xl tracking-tighter">
+          Everything you need
+        </h2>
+        <div className="max-w-xl mx-auto">
+          <p className="text-xl mt-5 text-white/70 text-center">
+            Our platform is designed to help you achieve your goals with ease.
+            Explore our features and see how we can help you succeed.{" "}
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row gap-4 mt-16">
+          {features.map(({ title, description }) => (
+            <div
+              key={title}
+              className="border border-white/30 py-10 px-5 rounded-xl text-center sm:flex-"
+            >
+              <div className="bg-white text-black inline-flex w-14 h-14 items-center justify-center rounded-lg">
+                <EcosystemIcon />
+              </div>
+              <h3 className="mt-6 font-bold">{title}</h3>
+              <p className="mt-2 text-white/70">{description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
